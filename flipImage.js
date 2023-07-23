6942de317b7a3aa0fa6178fe5f414748299d2d95
Stephen <gstephenc@hotmail.com>
2023-07-23 18:18:31 -0400

    created duplicates and button functions
    - use method to make up arrow design on the rest of the arrows

--- flipImage.js
+++ flipImage.js
@@ -1,9 +1,15 @@
-function FlipImage(axis){
-    const i = document.getElementById('image');
+function FlipImage(axis, img){
+    const i = document.getElementById(img);
     if(axis == "x"){
         i.style.transform = "scaleX(1)";
     }
     else if (axis == "-x"){
         i.style.transform = "scaleX(-1)";
+    }
+    else if (axis == "y"){
+        i.style.transform = "scaleY(1)";
+    }
+    else if (axis == "-y"){
+        i.style.transform = "scaleY(-1)";
     }
 }
