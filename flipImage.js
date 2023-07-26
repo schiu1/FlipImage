ea7356b59226d3689c366bf11532ae8d73f23fa9
Stephen <gstephenc@hotmail.com>
2023-07-26 12:50:22 -0400

    flip buttons work but vertical is at wrong origin
    - need to adjust the transform-origin of vertical flip
    - need to repeat for rest of images

--- flipImage.js
+++ flipImage.js
@@ -1,15 +1,23 @@
+let flippedX = false;
+let flippedNegX = false;
+let flippedY = false;
+let flippedNegY = false;
 function FlipImage(axis, img){
     const i = document.getElementById(img);
-    if(axis == "x"){
-        document.querySelector(".flipContainer").classList.toggle("flip");
+    if(axis == "x" && !flippedNegX && !flippedY & !flippedNegY){
+        document.querySelector(".flipContainer").classList.toggle("flipX");
+        flippedX = !flippedX;
     }
-    else if (axis == "-x"){
-        i.style.transform = "rotateY(-180deg)";
+    else if (axis == "-x" && !flippedX && !flippedY & !flippedNegY){
+        document.querySelector(".flipContainer").classList.toggle("flip-X");
+        flippedNegX = !flippedNegX;
     }
-    else if (axis == "y"){
-        i.style.transform = "scaleY(1)";
+    else if (axis == "y" && !flippedNegX && !flippedX & !flippedNegY){
+        document.querySelector(".flipContainer").classList.toggle("flipY");
+        flippedY = !flippedY;
     }
-    else if (axis == "-y"){
-        i.style.transform = "scaleY(-1)";
+    else if (axis == "-y" && !flippedNegX && !flippedX & !flippedY){
+        document.querySelector(".flipContainer").classList.toggle("flip-Y");
+        flippedNegY = !flippedNegY;
     }
 }
