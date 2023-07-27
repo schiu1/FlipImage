91c89d1c32f9bd60bc5789a82d1156bb9a2d0613
Stephen <gstephenc@hotmail.com>
2023-07-27 11:52:00 -0400

    found method of not "current flip" to not be shared
    - repeat for rest of images

--- flipImage.js
+++ flipImage.js
@@ -2,10 +2,26 @@ let flippedX = false;
 let flippedNegX = false;
 let flippedY = false;
 let flippedNegY = false;
+
+const history = new Map();
+history.set(".flipContainer", "");
+history.set(".flipContainer2", "");
+history.set(".flipContainer3", "");
+history.set(".flipContainer4", "");
+
 function FlipImage(axis, container){
-    if(axis == "x" && !flippedNegX && !flippedY & !flippedNegY){
+    /*if(axis == "x" && !flippedNegX && !flippedY & !flippedNegY){
         document.querySelector(container).classList.toggle("flipX");
         flippedX = !flippedX;
+    }*/
+    if(axis == "x" && (history.get(container) == "" || history.get(container) == "x")){
+        document.querySelector(container).classList.toggle("flipX");
+        if(history.get(container) == ""){
+            history.set(container, "x");
+        }
+        else if (history.get(container) == "x"){
+            history.get(container, "");
+        }
     }
     else if (axis == "-x" && !flippedX && !flippedY & !flippedNegY){
         document.querySelector(container).classList.toggle("flip-X");
